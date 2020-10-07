8cf7daa7501ce7600860bb51ecd27e2f56bf9b6f
Hamed Shahghasemi <shaghasemi@gmail.com>
2020-10-07 19:45:20 +0330

    Seperate Data File

---
+++ app/src/main/java/com/example/newsfeed/data/dataData.kt
@@ -0,0 +1,165 @@
+package com.example.newsfeed.data
+
+import com.example.newsfeed.R
+import com.example.newsfeed.model.NewsDataModel
+
+object dataData {
+    val newsData = listOf(
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        ),
+        NewsDataModel(
+            "Fake Title",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+                    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat" +
+                    " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id" +
+                    " est laborum.",
+            "Fake Date and Time",
+            R.drawable.placeholder_image
+        )
+    )
+}

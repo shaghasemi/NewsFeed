63c57d962886d84abc6e15518d907e126cec4eca
Hamed Shahghasemi <shaghasemi@gmail.com>
2020-10-07 20:15:52 +0330

    Starting to Retrofit

--- app/src/main/java/com/example/newsfeed/MainActivity.kt
+++ app/src/main/java/com/example/newsfeed/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.newsfeed
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsfeed.adapter.NewsMainAdapter
 import com.example.newsfeed.data.dataData.newsData
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var retrofit: Retrofit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        retrofit = Retrofit.Builder().build()
+//        var retrofit = retrofit.newBuilder().build()
 
         rv_main.adapter = NewsMainAdapter(newsData)
         rv_main.layoutManager = LinearLayoutManager(applicationContext)
---
+++ app/src/main/java/com/example/newsfeed/MyApi.kt
@@ -0,0 +1,8 @@
+package com.example.newsfeed
+
+import retrofit2.http.GET
+
+interface MyApi {
+//    @GET("http://newsapi.org/v2/top-headlines?country=us&apiKey=83f22a5d248b49ccb0cc392687bcd061")
+
+}
